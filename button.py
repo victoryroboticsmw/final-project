469d5f8d24a3a80c79bd9a07484fd56fcdf5018a
victoryroboticsmw <46609529+victoryroboticsmw@users.noreply.github.com>
2019-04-12 14:51:29 -0500

    Update button.py

--- button.py
+++ button.py
@@ -10,8 +10,7 @@ while True:
     button.when_pressed = green.on  
     button.when_released = green.off
     button.when_pressed = red.off
-    button.when_released = red
-    on
+    button.when_released = red.on
     red.on()
     sleep(1)
     red.off()
